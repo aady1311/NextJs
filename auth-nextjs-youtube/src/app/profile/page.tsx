a82a36222be82bfc5199068980b66a3ccca9630c
Aditya Yadav <sarry7045@gmail.com>
2025-11-08 15:13:06 +0530

    added middleware and me route

--- auth-nextjs-youtube/src/app/profile/page.tsx
+++ auth-nextjs-youtube/src/app/profile/page.tsx
@@ -1,9 +1,52 @@
+"use client";
+import axios, { Axios } from "axios";
+import Link from "next/link";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+
 export default function ProfilePage() {
+  const router = useRouter();
+  const [data, setData] = useState("Northing");
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logout successful");
+      router.push("/login");
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
       <hr />
       <p>Profile Page</p>
+      <h2 className="p-1 rounded bg-green-600">{data === "northing" ? "Northing" : <Link href={`\profile/${data}`}>{data}</Link>}</h2>
+      <hr />
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
+      >        Logout
+      </button>
+
+
+      <button
+        onClick={getUserDetails}
+        className="bg-green-800 hover:bg-blue-700 mt-4 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
+      </button>
     </div>
   );
 }
